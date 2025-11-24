f2b34ef10409419fa8e2cd0ef8fd8542b35096bb
funayamateppei <t.funayama0217@gmail.com>
2025-11-24 19:32:15 +0900

    chore: move file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { MainLayout } from './layouts/MainLayout'
-import { HomePage } from './pages/HomePage'
+import { MainLayout } from './presentation/layouts/MainLayout'
+import { HomePage } from './presentation/pages/HomePage'
 
 function App() {
   return (
